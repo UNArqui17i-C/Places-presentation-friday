4a24d9f98cfaa10ea344c0182f19c1db3bde6131
jaavargasar <jaavargasar@gmail.com>
2017-05-22 22:33:23 -0500

    add scale 2.0

--- start2.sh
+++ start2.sh
@@ -5,7 +5,7 @@
     --url http://192.168.99.100:8080/v1/projects/1a5 \
     --access-key A7E8F72835B8DECE8C31 \
     --secret-key shvVdzeasbRs3WgopQconrm4DwLMZcpYEG8TVTy8 \
-    -f docker-compose.prod.yml \
+    -f docker-compose.yml \
     --verbose up \
     -d --force-upgrade \
     --confirm-upgrade
